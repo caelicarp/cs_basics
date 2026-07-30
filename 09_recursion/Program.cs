4ae37d546ea565573eeee92a3006417f44b7e3b4
caeli <90215010+caelicarp@users.noreply.github.com>
2026-07-30 01:18:19 -0600

    renamed project folders and added exercises

---
+++ 09_recursion/Program.cs
@@ -0,0 +1,14 @@
+﻿void RocketLauncher(int ignition)
+{
+  if (ignition <= 0)
+  {
+    Console.WriteLine("Ignition!");
+    return; //return sin valor porque el método es void
+  }
+
+  Console.WriteLine(ignition);
+
+  RocketLauncher(ignition - 1); //cuenta regresiva
+}
+
+RocketLauncher(3);
