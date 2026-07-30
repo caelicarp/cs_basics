235cd28d1c40e7fcbf66d5fa4d6ad5225741e6b0
caeli <90215010+caelicarp@users.noreply.github.com>
2026-07-30 01:26:50 -0600

    renamed project folders and added exercises

---
+++ 07_return_from_methods/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("How high should I count?");
+int chosenNumber = ReadNumber();
+Count(chosenNumber);
+
+void Count(int numberToCount)
+{
+  for (int current = 1; current <= numberToCount; current++)
+  {
+    Console.WriteLine(current);
+  }
+}
+
+int ReadNumber()
+{
+  string input = Console.ReadLine();
+  int number = Convert.ToInt32(input);  
+  return number;
+}
+
+
---
+++ 08_returning_early/Program.cs
@@ -0,0 +1,17 @@
+﻿string GetUserName()
+{
+  while (true)
+  {
+    Console.WriteLine("What's your name?");
+    string name = Console.ReadLine();
+
+    if (name != "")
+    {
+      return name;
+    }
+
+    Console.WriteLine("Let's try again");
+  }
+}
+
+GetUserName();
